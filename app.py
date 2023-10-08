d5a5d99083ef5923c2e631318f0dfc98631886e5
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-08 11:55:46 +0530

    mvp created

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from main import FamilyExpenseTracker
+
+# Streamlit configuration
+st.set_page_config(
+    page_title="Family Expense Tracker",
+    page_icon="💰",
+    layout="wide"
+)
+
+st.title("Family Expense Tracker")
+
+# Create a session state object
+session_state = st.session_state
+
+# Check if the 'expense_tracker' object exists in the session state
+if 'expense_tracker' not in session_state:
+    # If not, create and initialize it
+    session_state.expense_tracker = FamilyExpenseTracker()
+
+# Sidebar for adding family members
+st.sidebar.header("Add Family Member")
+member_name = st.sidebar.text_input("Name")
+earning_status = st.sidebar.checkbox("Earning Status")
+earnings = st.sidebar.number_input("Earnings", value=0)
+
+# Access the 'expense_tracker' object from session state
+expense_tracker = session_state.expense_tracker
+
+if st.sidebar.button("Add Member"):
+    expense_tracker.add_family_member(member_name, earning_status, earnings)
+    st.sidebar.success("Member added successfully!")
+    
+# Sidebar for managing expenses
+st.sidebar.header("Manage Expenses")
+expenses = st.sidebar.number_input("Expenses", value=0)
+
+if st.sidebar.button("Deduct Expenses"):
+    remaining_balance = expense_tracker.deduct_expenses(expenses)
+    st.sidebar.success(f"Expenses deducted successfully! Remaining Balance: {remaining_balance}")
+    
+# Display family members
+st.header("Family Members")
+for member in expense_tracker.members:
+    st.write(f"Name: {member.name}, Earning Status: {'Earning' if member.earning_status else 'Not Earning'}, Earnings: {member.earnings}")
+
+# Display total earnings
+total_earnings = expense_tracker.calculate_total_earnings()
+st.header("Total Earnings")
+st.write(f"Total Earnings: {total_earnings}")
+
+# Display remaining balance
+remaining_balance = total_earnings - expenses
+st.header("Remaining Balance")
+st.write(f"Remaining Balance: {remaining_balance}")
---
+++ main.py
@@ -0,0 +1,31 @@
+# main.py
+
+class FamilyMember:
+    def __init__(self, name, earning_status=True, earnings=0):
+        self.name = name
+        self.earning_status = earning_status
+        self.earnings = earnings
+
+    def __str__(self):
+        return f"Name: {self.name}, Earning Status: {'Earning' if self.earning_status else 'Not Earning'}, Earnings: {self.earnings}"
+
+class FamilyExpenseTracker:
+    def __init__(self):
+        self.members = []
+
+    def add_family_member(self, name, earning_status=True, earnings=0):
+        member = FamilyMember(name, earning_status, earnings)
+        self.members.append(member)
+
+    def calculate_total_earnings(self):
+        total_earnings = sum(member.earnings for member in self.members if member.earning_status)
+        return total_earnings
+
+    def deduct_expenses(self, expenses):
+        total_earnings = self.calculate_total_earnings()
+        remaining_balance = total_earnings - expenses
+        return remaining_balance
+
+if __name__ == "__main__":
+    expense_tracker = FamilyExpenseTracker()
+
