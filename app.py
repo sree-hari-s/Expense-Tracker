9bb9a625acfc379e60ec3efdcea4e55cd492092d
Acidix <Aleksanderbo10@gmail.com>
2023-10-10 18:16:46 +0200

    Track each expense instead of total expense

--- app.py
+++ app.py
@@ -38,11 +38,14 @@ if st.sidebar.button("Add Member"):
         
 # Sidebar for managing expenses
 st.sidebar.header("Manage Expenses")
-expenses = st.sidebar.number_input("Expenses", value=0, min_value=0)
+expense_categories = ["Housing", "Transportation", "Food", "Healthcare", "Other"]
+selected_expense_category = st.sidebar.selectbox("Select an category", expense_categories)
+expense_name = st.sidebar.text_input("Expense name")
+expense_description = st.sidebar.text_input("Enter a description")
+expense_cost = st.sidebar.number_input("Cost", value=0, min_value=0)
 
-if st.sidebar.button("Deduct Expenses"):
-    remaining_balance = expense_tracker.deduct_expenses(expenses)
-    st.sidebar.success(f"Expenses deducted successfully! Remaining Balance: {remaining_balance}")
+if st.sidebar.button("Add expense"):
+    expense_tracker.add_expense(selected_expense_category, expense_name,expense_description, expense_cost)
 
 # Display family members
 st.header("Family Members")
@@ -57,12 +60,26 @@ for member in expense_tracker.members:
     earning_status_column.write('Earning' if member.earning_status else 'Not Earning')
     earnings_column.write(member.earnings)
 
-# Display total earnings
-total_earnings = expense_tracker.calculate_total_earnings()
-st.header("Total Earnings")
-st.write(f"Total Earnings: {total_earnings}")
 
-# Display remaining balance
-remaining_balance = total_earnings - expenses
-st.header("Remaining Balance")
-st.write(f"Remaining Balance: {remaining_balance}")
+# Display all cost
+st.header("Expenses")
+expense_category_column, expense_name_column, expense_cost_column, expense_description_column = st.columns(4)
+expense_category_column.write("**Category**")
+expense_name_column.write("**Name**")
+expense_cost_column.write("**Cost**")
+expense_description_column.write("**Description**")
+
+for expense in expense_tracker.expenses:
+    expense_category_column.write(expense.category)
+    expense_name_column.write(expense.name)
+    expense_cost_column.write(expense.cost)
+    expense_description_column.write(expense.description)
+
+total_earnings = expense_tracker.calculate_total_earnings()
+st.header(f"Total Earnings: {total_earnings}")
+
+total_cost = expense_tracker.calculate_total_expenses()
+st.header(f"Total Cost: {total_cost}")
+
+net_income = total_earnings - total_cost
+st.header(f"Net Income: {net_income}")
--- main.py
+++ main.py
@@ -7,9 +7,17 @@ class FamilyMember:
     def __str__(self):
         return f"Name: {self.name}, Earning Status: {'Earning' if self.earning_status else 'Not Earning'}, Earnings: {self.earnings}"
 
+class Expense:
+    def __init__(self, category, name, description="", cost=0):
+        self.category = category
+        self.name = name
+        self.cost = cost
+        self.description = description  
+
 class FamilyExpenseTracker:
     def __init__(self):
         self.members = []
+        self.expenses = []
 
     def add_family_member(self, name, earning_status=True, earnings=0):
         if not name.strip():
@@ -18,9 +26,17 @@ class FamilyExpenseTracker:
         member = FamilyMember(name, earning_status, earnings)
         self.members.append(member)
 
+    def add_expense(self, category, name, description, cost):
+        expense = Expense(category, name, description, cost)
+        self.expenses.append(expense)
+
     def calculate_total_earnings(self):
         total_earnings = sum(member.earnings for member in self.members if member.earning_status)
         return total_earnings
+    
+    def calculate_total_expenses(self):
+        total_expenses = sum(expense.cost for expense in self.expenses)
+        return total_expenses
 
     def deduct_expenses(self, expenses):
         total_earnings = self.calculate_total_earnings()
